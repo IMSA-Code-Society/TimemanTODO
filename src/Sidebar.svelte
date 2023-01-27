8c17d294e10afb7942bd5a8eea3d178bffda3c1e
nlaud <45825432+nlaud@users.noreply.github.com>
2023-01-26 20:22:03 -0600

    added framework in svelte for sidebar.

---
+++ src/Sidebar.svelte
@@ -0,0 +1,34 @@
+<style>
+    
+    .container {
+        margin: auto;
+        font-family: sans-serif;
+        display:flex;
+    }
+    .sidebar{
+        display: flex;
+        flex:1;
+        padding: 5px;
+        flex-direction: column;
+        align-self:flex-start;
+    }
+    .other{
+        flex:3;
+        flex-direction: column;
+        display: flex;
+    }
+</style>
+<div class="container">
+    <div class="sidebar">
+        <h1>Sidebar</h1>
+        <br><br>
+        <h3>Projects</h3>
+        <!-- Add actual functionality for this later-->
+        <ul>
+            <li>Thing</li>
+            <li>Thing</li>
+            <li>Thing</li>
+        </ul>
+    </div>
+    <div class="other"><slot></slot></div>
+</div>
