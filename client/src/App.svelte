d90715b12347b52d9efb02ca176d5dbb0d339e46
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-04-06 09:48:04 -0500

    Add SPA routing

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
-    import Tasks from "./tasks/Tasks.svelte";
-    import Sidebar from "./Sidebar.svelte";
+  import Router from 'svelte-spa-router';
+  import {wrap} from 'svelte-spa-router/wrap'
+  import Home from "./routes/home/Home.svelte";
+  import NotFound from "./routes/NotFound.svelte";
+
+  const routes = {
+    '/': Home,
+
+    // Wrapping the Author component
+    // '/author/:first/:last?': wrap({
+    //   asyncComponent: () => import('./routes/Author.svelte')
+    // }),
+    //
+    // // Wrapping the Book component
+    // '/book/*': wrap({
+    //   asyncComponent: () => import('./routes/Book.svelte')
+    // }),
+
+    // Catch-all route last
+    '*': NotFound,
+  }
 </script>
 
-<Sidebar><Tasks /></Sidebar>
+<Router {routes}/>
