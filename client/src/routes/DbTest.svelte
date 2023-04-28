cbb9cbe3278a3ec062c75b7e6cbec79b55f63bc7
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-04-27 21:47:09 -0500

    Make general stateful db accessor

--- client/src/routes/DbTest.svelte
+++ client/src/routes/DbTest.svelte
@@ -6,6 +6,7 @@
   import type {Readable, Subscriber, Unsubscriber} from "svelte/store";
   import type {SavedTask} from "../lib/api";
   import {writable} from "svelte/store";
+    import type PouchDb from 'pouchdb-browser';
 
   PouchDB.plugin(DeltaPouch);
   // PouchDB.plugin(PouchFind);
@@ -26,12 +27,46 @@
   //   };
   // });
 
+  // jk this was a bad idea to get type info
+  // class ReactivePouchDb<Schema> extends PouchDB<Schema> {
+  //   constructor(name?: string, options?: PouchDB.Configuration.DatabaseConfiguration) {
+  //     super(name, options);
+  //   }
+  // }
+
+  // Extend prototype with generics according to https://stackoverflow.com/a/52514102
+  interface PouchDB<T> {
+    asSvelteStore(): Readable<T>;
+  }
+
+  PouchDB.plugin(PouchDB => {
+    // type Schema = typeof PouchDB extends PouchDB.Database<infer Item> ? Item : typeof PouchDB;
+    PouchDB.prototype.asSvelteStore = function<T>(): Readable<T[]> {
+      console.log(this);
+      // Is a mapping of id to doc
+      let state = {};
+      const self = this;
+      this.deltaInit();
+  
+      return {
+        subscribe(run: Subscriber<T[]>, invalidate: Invalidator<T[]> | undefined): Unsubscriber {
+          run([]);
+          db.all().then(docs => {
+            state = docs;
+            notify();
+          });
+          const notify = () => { console.log(state); run(Object.values(state)); };
+          self.delta
+            .on('create', create => { state[create.$id] = create; notify(); })
+            .on('update', changes => { state[changes.$id] = {...state[changes.$id], ...changes}; notify(); })
+            .on('delete', id => { delete state[id]; notify(); });
+          return () => {};  // TODO: actually clean up
+        }
+      }
+    };
+  });
+
   const db = new PouchDB<{}>("test");
-  db.deltaInit();
-  db.delta
-    .on('create', refetch)
-    .on('update', refetch)
-    .on('delete', refetch);
 
   // const liveFeed = db.liveFindStore({
   //   selector: {},  // {series: 'Mario'}
@@ -39,20 +74,12 @@
   //   aggregate: true
   // });
 
-  const liveFeed = writable([]);
-
-  function refetch() {
-    db.all().then(function (docs) {
-      console.log(docs);
-      liveFeed.set(Object.values(docs));
-    });
-  }
-  refetch();
+  const liveFeed = db.asSvelteStore();
 
   let newTodo: string;
 
-  function submit(id=new Date().getTime(), title=undefined) {
-      db.save({ title: title ?? newTodo }).then(function (doc) {
+  function submit(id=undefined, title=undefined) {
+      db.save({ $id: id, title: title ?? newTodo }).then(function (doc) {
         console.log(doc);
         // doc.$id is the id of the created doc
       });
