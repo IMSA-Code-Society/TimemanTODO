0fdc732fbe3759ec74f1f0da6ced4d5dcd3b3b54
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-05-02 23:47:24 -0500

    Add search ability

--- client/src/db/asSvelteStore.ts
+++ client/src/db/asSvelteStore.ts
@@ -5,30 +5,42 @@
 
 import type {Readable, Subscriber, Unsubscriber} from "svelte/types/runtime/store";
 import type PouchDBType from "pouchdb-browser";
+import {writable} from "svelte/store";
+import type {RequestDef} from "../global";
 
 function asSvelteStore(PouchDB: typeof PouchDBType) {
-  PouchDB.prototype.asSvelteStore = function<T>(): Readable<T[]> {
+  PouchDB.prototype.asSvelteStore = function<T>(requestDef?: RequestDef<T>) {
     const db = this;
-    // Is a mapping of id to doc
-    let state = {};
-    const self = this;
-    this.deltaInit();
+    // Is a mapping of id to doc TODO: confirm extra props like _id $id _rev $modifiedAt
+    let state: Record<string, T>;
+    // Check !set b/c no parameters passed when ending, but passed when starting
+    const store = writable([] as T[], set => {if (!set) db.close()});
+    const notify = () => { console.log(state); store.set(Object.values(state)); };
+    db.deltaInit();
+    db.delta
+      .on('create', create => { state[create.$id] = create; notify(); })
+      .on('update', changes => { state[changes.$id] = {...state[changes.$id], ...changes}; notify(); })
+      .on('delete', id => { delete state[id]; notify(); });
 
-    return {
-      subscribe(run: Subscriber<T[]>, invalidate: Invalidator<T[]> | undefined): Unsubscriber {
-        run([]);
-        db.all().then(docs => {
-          state = docs;
-          notify();
-        });
-        const notify = () => { console.log(state); run(Object.values(state)); };
-        self.delta
-          .on('create', create => { state[create.$id] = create; notify(); })
-          .on('update', changes => { state[changes.$id] = {...state[changes.$id], ...changes}; notify(); })
-          .on('delete', id => { delete state[id]; notify(); });
-        return () => {};  // TODO: actually clean up
+    async function updateSearch(newRequestDef?: RequestDef<T>) {
+      if (newRequestDef) {
+        state = {};
+        const {docs, warning} = await db.find(newRequestDef);
+        if (warning)
+          console.warn(warning);
+        docs.forEach(el => state[el.$id] = el);
+      } else {
+        state = await db.all();
       }
+      notify();
     }
+    updateSearch(requestDef);
+
+    // Plot twist: readable is just a wrapped writable. Therefore, don't try to implement your own "store"
+    return {
+      subscribe: store.subscribe,
+      updateSearch,
+    } as Readable<T[]> & {updateSearch: typeof updateSearch};  // Cast to more readable format
   };
 }
 
--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="svelte" />
-/// <reference types="pouchdb" />
+/// <reference types="pouchdb-browser" />
 /// <reference types="pouchdb-find" />
 /// <reference types="pouchdb-live-find" />
+
+// Must be in global.d.ts b/c complaints about isolatedModules & preserveValueImports
+export type RequestDef<T> = PouchDB.LiveFind.RequestDef<T>;
 
 declare type Invalidator<T> = (value?: T) => void;
