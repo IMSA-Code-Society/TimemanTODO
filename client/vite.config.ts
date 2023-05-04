0466ed1031bc32c1105739d3f9e9cc47112a6dc6
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-05-03 21:34:20 -0500

    grr Vite being dumb

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode, ssrBuild }) => ({
   plugins: [svelte()],
   define: {
     // Vite doesn't include shims for Node globals, & pouchDb dependency uses it (see https://github.com/calvinmetcalf/immediate/pull/40)
-    global: {},
+    global: mode === "development" ? {} : "global",
     // If there are still issues, try Object.fromEntries(Object.entries(globalThis).filter(([key, val]) => val !== globalThis))
   }
-})
+}))
