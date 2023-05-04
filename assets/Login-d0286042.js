7be5d9f2feb4d60bb10a16f91e0d7502827ed356
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-05-04 03:11:26 +0000

    Deploying to gh-pages from @ IMSA-Code-Society/TimemanTODO@7883ca392101c3d41d8b3b691396c76cf95dad73 🚀

---
+++ assets/Login-d0286042.js
@@ -0,0 +1,6 @@
+import{S as y,i as g,s as x,e as o,a as _,b as l,c as p,d as c,D as L,n as u,f as b}from"./index-8c1b18b3.js";function w(v){let e,i,s,a,d,n,r,m;return{c(){e=o("link"),i=_(),s=o("body"),a=o("a"),a.innerHTML='<button class="back svelte-1ragfpe"><i class="fa fa-arrow-left"></i></button>',d=_(),n=o("div"),n.innerHTML=`<h1>Login</h1> 
+        <form action=""><label for="uname">Username</label><br/> 
+            <input type="text" id="uname"/><br/><br/> 
+            <label for="pass">Password</label><br/> 
+            <input type="password" id="pass"/><br/><br/> 
+            <input type="submit" value="Login"/></form>`,l(e,"rel","stylesheet"),l(e,"href","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"),l(a,"class","loginButton"),l(a,"href",""),l(n,"class","loginBox center svelte-1ragfpe"),l(s,"class","svelte-1ragfpe")},m(t,f){p(t,e,f),p(t,i,f),p(t,s,f),c(s,a),c(s,d),c(s,n),r||(m=L(link.call(null,a)),r=!0)},p:u,i:u,o:u,d(t){t&&b(e),t&&b(i),t&&b(s),r=!1,m()}}}class k extends y{constructor(e){super(),g(this,e,null,w,x,{})}}export{k as default};
